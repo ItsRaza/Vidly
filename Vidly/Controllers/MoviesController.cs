d82e30ef7743458da5509eab11827dfba90b360b
ItsRaza <razaabbas768@gmail.com>
2019-07-17 23:46:50 +0500

    Movies and Customers controllers added and updated. Added hardcoded Movies and customers to the index views of Movies and customers

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+            
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1,Name="Avengers Infinity War" },
+                new Movie {Id=1,Name="Avengers EndGame" }
+            };
+        }
+    }
+}
